4c78a722dfbea65a226aad43af49ed694bb264d4
Saumya Sharma <saumyasharma010@gmail.com>
2020-08-12 13:04:17 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://saumya:Saumya%40123%23@cluster0-ui0c6.mongodb.net/auth?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://password@cluster.mongodb.net/auth?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
